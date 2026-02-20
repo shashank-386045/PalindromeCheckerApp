69dc8a0fc61f083b2fac20cbd924cdbde3adc382
N Shashank <shashank.nuthalapati06@gmail.com>
2026-02-20 10:07:06 +0530

    UC3:Palindrome check using string reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,47 +1,36 @@
-// UseCase2PalindromeCheckerApp.java
+
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+    public class PalindromeCheckerApp {
 
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=======================================");
-        System.out.println("   Palindrome Checker App - Use Case 2 ");
-        System.out.println("=======================================");
-        System.out.print("Enter a word to check: ");
+            System.out.println("=======================================");
+            System.out.println("   Palindrome Checker App - Use Case 3 ");
+            System.out.println("=======================================");
+            System.out.print("Enter a word to check: ");
 
 
-        String word = scanner.nextLine();
+            String original = scanner.nextLine();
 
-      ma
-        if (isPalindrome(word)) {
-            System.out.println("Result: \"" + word + "\" is a palindrome.");
-        } else {
-            System.out.println("Result: \"" + word + "\" is NOT a palindrome.");
-        }
 
-        System.out.println("---------------------------------------");
-        System.out.println("Program exited successfully.");
-
-        // Close scanner to avoid resource leak
-        scanner.close();
-    }
-
-    // Helper method to check palindrome
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
+            String reversed = "";
+            for (int i = original.length() - 1; i >= 0; i--) {
+                reversed = reversed + original.charAt(i); // String concatenation
             }
-            left++;
-            right--;
+
+
+            if (original.equals(reversed)) {
+                System.out.println("Result: \"" + original + "\" is a palindrome.");
+            } else {
+                System.out.println("Result: \"" + original + "\" is NOT a palindrome.");
+            }
+
+            System.out.println("---------------------------------------");
+            System.out.println("Program exited successfully.");
+
+            scanner.close();
         }
-        return true;
     }
-}
