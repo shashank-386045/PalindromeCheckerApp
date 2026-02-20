3c0dc71bd83b01238be67d23e383eb7ed5025e91
N Shashank <shashank.nuthalapati06@gmail.com>
2026-02-20 10:24:56 +0530

    UC4:using character using palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,8 +3,6 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-
-    // main method: entry point of the application
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,14 +10,8 @@ public class PalindromeCheckerApp {
         System.out.println("   Palindrome Checker App - Use Case 4 ");
         System.out.println("=======================================");
         System.out.print("input:");
-
-        // Take input from user
         String word = scanner.nextLine();
-
-        // Convert string to character array
         char[] charArray = word.toCharArray();
-
-        // Two-pointer approach
         int left = 0;
         int right = charArray.length - 1;
         boolean isPalindrome = true;
@@ -32,8 +24,6 @@ public class PalindromeCheckerApp {
             left++;
             right--;
         }
-
-        // Display result
         if (isPalindrome) {
             System.out.println("Is a palindrome?:True");
         } else {
