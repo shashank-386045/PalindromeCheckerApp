e45fb1a8d0a683bd91f526baeb51fd0e275283a1
N Shashank <shashank.nuthalapati06@gmail.com>
2026-02-20 10:39:46 +0530

    UC5:Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,38 +1,30 @@
 
-
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("=======================================");
-        System.out.println("   Palindrome Checker App - Use Case 4 ");
+        System.out.println("   Palindrome Checker App - Use Case 5 ");
         System.out.println("=======================================");
-        System.out.print("input:");
-        String word = scanner.nextLine();
-        char[] charArray = word.toCharArray();
-        int left = 0;
-        int right = charArray.length - 1;
-        boolean isPalindrome = true;
-
-        while (left < right) {
-            if (charArray[left] != charArray[right]) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
+        System.out.print("Input:");
+        String input = scanner.nextLine();
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
         }
-        if (isPalindrome) {
-            System.out.println("Is a palindrome?:True");
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+        if (input.equals(reversed)) {
+            System.out.println("Is it palindrome?:True");
         } else {
-            System.out.println("Is a palindrome?:False");
+            System.out.println("Is it Palindrome?:False");
         }
-
         System.out.println("---------------------------------------");
         System.out.println("Program exited successfully.");
-
         scanner.close();
     }
 }
